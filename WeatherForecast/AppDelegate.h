1f5a7e3c2d7b868c6e9a4080c68945accfc50ba9
ZoroF <fengky1985@163.com>
2015-11-03 23:21:21 +0800

    Initial Commit

---
+++ WeatherForecast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherForecast
+//
+//  Created by Zoro on 15/11/3.
+//  Copyright © 2015年 Zoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
